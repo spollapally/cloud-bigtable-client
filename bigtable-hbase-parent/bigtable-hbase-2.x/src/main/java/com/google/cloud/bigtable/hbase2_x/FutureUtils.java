709cdbee9327c1d39d657021993cd2e3c1239419
Sudharshan Pollapally <spollapally@udbhavinc.com>
2017-12-08 09:05:05 -0500

    added BigtableAsyncTableRegionLocator

---
+++ bigtable-hbase-parent/bigtable-hbase-2.x/src/main/java/com/google/cloud/bigtable/hbase2_x/FutureUtils.java
@@ -0,0 +1,38 @@
+package com.google.cloud.bigtable.hbase2_x;
+
+import java.util.concurrent.CompletableFuture;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
+/**
+ * 
+ * @author spollapally
+ */
+public class FutureUtils {
+
+  public static <T> CompletableFuture<T>
+      toCompletableFuture(final ListenableFuture<T> listenableFuture) {
+    final CompletableFuture<T> completableFuture = new CompletableFuture<T>() {
+      @Override
+      public boolean cancel(boolean mayInterruptIfRunning) {
+        boolean result = listenableFuture.cancel(mayInterruptIfRunning);
+        super.cancel(mayInterruptIfRunning);
+        return result;
+      }
+    };
+
+    Futures.addCallback(listenableFuture, new FutureCallback<T>() {
+      public void onFailure(Throwable throwable) {
+        completableFuture.completeExceptionally(throwable);
+      }
+
+      public void onSuccess(T t) {
+        completableFuture.complete(t);
+      }
+    });
+    return completableFuture;
+  }
+
+}
